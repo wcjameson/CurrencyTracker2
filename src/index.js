3f92eb2d7ac93feb7d6221086133465978ddbce0
wcjameson <williamjameson90@gmail.com>
2022-02-11 14:46:59 -0800

    add formula to calculate currency exchange, project has baseline functionality

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import './css/styles.css';
 import CurrencyExchange from './currency-exchange.js';
 
 $(document).ready(function() {
-  $('#exchange').submit(function() {
+  $('#exchange').submit(function(event) {
+    event.preventDefault();
     let currencyUSD = $('#currencyUSD').val();
     let currencyExchange = $('#currencyExchange').val();
     let promise = CurrencyExchange.getCurrencyExchange(currencyExchange);
     promise.then(function(response) {
       const body = JSON.parse(response);
-      let newAmount = (`${body.conversionrate}`) * currencyUSD;
+      let newAmount = ((`${body.conversion_rate}`) * currencyUSD);
+      console.log(response);
       console.log(newAmount);
-      $('#showCurrencyExchange').text(` ${currencyUSD} USD converted to ${currencyExchange} is ${newAmount}`);
+      $('.showCurrencyExchange').text('');
+      $('.showCurrencyExchange').append(newAmount);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
     });
